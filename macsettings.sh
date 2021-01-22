e36072e23b4f9d32d97c813c632e6411cc3bd3d6
Keaton Boyle <keatonboyle@gmail.com>
2021-01-21 21:57:08 -0500

    Turn off two-finger 'back' in Chrome

--- macsettings.sh
+++ macsettings.sh
@@ -3,3 +3,6 @@ defaults write com.apple.finder AppleShowAllFiles true
 
 # Always show scrollbars.
 defaults write -g AppleShowScrollBars -string "Always"
+
+# Don't use two-finger back/forward in Chrome
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
